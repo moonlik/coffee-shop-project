bd346c9bd84f395d4513d9f4dbd09f024f4ce9a0
moonlik <19203280+moonlik@users.noreply.github.com>
2021-06-09 15:29:18 +0800

    Extended tailwind variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,36 +2,44 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-		extend: {
+      extend: {
         fontFamily: {
           'sans': ['Raleway'],
           'serif': ['PlayfairDisplay'],
         },
-			backgroundImage: theme => ({
+        backgroundImage: theme => ({
           'hero-pattern': "url(./assets/images/coffee-house-header.jpeg)",
           'company-image': "url('./assets/images/coffee-house-interior.jpeg')"
-			}),
-      skew: {
-        '-25': '-25deg',
-       },
-      colors: {
-        brown: {
-          '50': '#F7DFB5',
-          '100': '#F5D59E',
-          '200': '#F0C170',
-          '300': '#EBAD42',
-          '400': '#E29818',
-          '500': '#B47913',
-          '600': '#865A0E',
-          '700': '#583B09',
-          '800': '#2A1C04',
-          '900': '#000000'
+        }),
+        skew: {
+          '-25': '-25deg',
+        },
+        scale: {
+          '-1': '-1',
+        },
+        colors: {
+          brown: {
+            '50': '#F7DFB5',
+            '100': '#F5D59E',
+            '200': '#F0C170',
+            '300': '#EBAD42',
+            '400': '#E29818',
+            '500': '#B47913',
+            '600': '#865A0E',
+            '700': '#583B09',
+            '800': '#2A1C04',
+            '900': '#000000'
+          }
         }
-      }
-		},
+      },
     },
     variants: {
-      extend: {},
+      extend: {
+        transform: ['hover'],
+        animation: ['hover', ],
+        translate: ['group-hover'],
+        scale: ['active'],
+      },
     },
     plugins: [],
   }
