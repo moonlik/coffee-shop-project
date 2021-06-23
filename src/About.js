fd8f931c5145040cdee73872ef397c634e6f5c7a
moonlik <19203280+moonlik@users.noreply.github.com>
2021-06-23 12:24:30 +0800

    Changed font size

--- src/About.js
+++ src/About.js
@@ -21,17 +21,17 @@ export default function About() {
                     <div className="xl:w-1/3 flex flex-wrap xl:px-0 m-5 pl-3 md:pl-16 xl:pl-0">
                         <div className={`opacity-0 m-6 md:m-10 ${isVisible && 'animate-fadein-bottom opacity-100'}`}>
                             <h3 className="text-2xl font-serif mb-3 text-yellow-500">Hours</h3>
-                            <p className="text-lg font-serif mb-1">Monday - Friday</p>
+                            <p className="text-xl font-serif mb-1">Monday - Friday</p>
                             <p>11 am - 4 pm</p>
                         </div>
                         <div className={`opacity-0 m-6 md:m-10 ${isVisible && 'animate-fadein-bottom opacity-100'}`}>
                             <h3 className="text-2xl font-serif mb-3 text-yellow-500">Launch</h3>
-                            <p className="text-lg font-serif mb-1">Monday - Friday</p>
+                            <p className="text-xl font-serif mb-1">Monday - Friday</p>
                             <p>11 am - 1 pm</p>
                         </div>
                         <div className={`opacity-0 m-6 md:m-10 ${isVisible && 'animate-fadein-bottom opacity-100'}`}>
                             <h3 className="text-2xl font-serif mb-3 text-yellow-500">Dinner</h3>
-                            <p className="text-lg font-serif mb-1">Monday - Friday</p>
+                            <p className="text-xl font-serif mb-1">Monday - Friday</p>
                             <p>1 pm - 4 pm</p>
                         </div>
                     </div>
